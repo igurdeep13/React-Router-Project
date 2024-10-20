ab289f0fd19b6868729b1fa9e948f1572a24aa66
Gurdeep Singh <g.s.basantpuria@gmail.com>
2024-10-20 00:46:46 -0400

    Add router in the code

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="bg-green-600 p-y">Hello world</h1>
+      <Header />
+      <Home />
+      <Footer />
     </>
   );
 }
