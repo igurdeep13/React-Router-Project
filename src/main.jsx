0df8bc5ff375896b8c55728772a153dac27b15eb
Gurdeep Singh <g.s.basantpuria@gmail.com>
2024-10-21 15:43:02 -0400

    Add router and components like about contact

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,27 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./Layout.jsx";
+import { Home, About, Contact } from "./components";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
